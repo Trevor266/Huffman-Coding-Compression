35e04fbae74ed32aa20fcb39c4949182dcc2e7e3
Trevor266 <Trevorhart66@yahoo.com>
2016-01-22 09:53:38 -0600

    Windows Build Start - BitIO.h

---
+++ Compression_Huffman_Coding/BitIO.h
@@ -0,0 +1,46 @@
+#ifndef _BITIO_H_
+#define _BITIO_H_
+
+#include <stdio.h>
+
+typedef struct bit_file
+{
+	FILE *file;
+	unsigned char mask;
+	int rack;
+	int pacifier_counter;
+} BIT_FILE;
+
+
+#ifdef __STDC__
+
+BIT_FILE*		OpenInputBitBuffer(char *name);
+BIT_FILE*		OpenOutputBitBuffer(char *name);
+void			OutputBit(BIT_FILE *bit_file, int bit);
+void			OutputBits(BIT_FILE *bit_file,
+	unsigned long code,
+	int count);
+
+int				InputBit(BIT_FILE *bit_file);
+unsigned long	InputBits(BIT_FILE *bit_file, int bit_count);
+void			CloseInputBitFile(BIT_FILE *bit_file);
+void			CloseOutputBitFile(BIT_FILE *bit_file);
+void			FilePrintBinary(FILE *file, unsigned int code, int bits);
+
+#else /*__STDC__*/
+
+BIT_FILE*		OpenInputBitFile();
+BIT_FILE*		OpenOutputBitFile();
+void			OutputBit();
+void			OutputBits();
+int				InputBit();
+unsigned long	InputBits();
+void			CloseInputBitFile();
+void			CloseOutputBitFile();
+void			FilePrintBinary();
+
+
+#endif /*__STDC__*/
+
+#endif /*_BITIO_H_*/
+
